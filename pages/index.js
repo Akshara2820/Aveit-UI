c28e05a401fe77382f3e373d34e3a3f0d7437c59
Frontend1 <fdev1.delta4@gmail.com>
2022-11-08 18:50:26 +0530

    fix error for deploy

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-
-import Header from "./component/header";
-import QuickAccess from "./component/QuickAccess";
-import TicketingSystem from "./component/ticketing-system";
-import FavoritePlateform from "./component/AllBrands";
-import OverView from "./component/overview";
-import Faq from "./component/faq";
-import Package from "./component/packages";
-import Review from "./component/review";
-import News from "./component/news";
-import Footer from "./component/footer";
 import Layout from "./component/layout";
 
 
